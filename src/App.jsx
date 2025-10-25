2be3998b0cafee08672a58a3edb757f05bc246cb
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-10-25 23:39:49 +0600

    Footer Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Menu from './pages/Menu';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/contact" element={<Contact/>} />
         <Route path="/menu" element={<Menu/>} />
       </Routes>
+      <Footer/>
       
     </main>
       
