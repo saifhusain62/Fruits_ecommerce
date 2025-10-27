f6238e268cb2aed641bc9e2fa8314e515eb060d6
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-10-27 22:09:28 +0600

    Homer Offersection Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/Heromain'
 import DiscoverSection from '../components/Discoversection'
+import Homeoffermenu from '../components/Homeoffermenu'
 
 function Home() {
   return (
     <div>
       <HeroSection/>
       <DiscoverSection/>
+      <Homeoffermenu/>
     </div>
   )
 }
